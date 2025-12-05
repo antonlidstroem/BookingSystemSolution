b9240ebcc5ce290a040507e3afee42b7cfcf3652
Anton Lidström <antonlidstroem@outlook.com>
2025-12-05 17:01:00 +0100

    Added Console and Test projects.

---
+++ BookingSystem.Console/Interface/IBookingService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Console.Interface
+{
+    public interface IBookingService
+    {
+        Task<List<Booking>> GetAllBookingsAsync();
+        Task<Booking> CreateBookingAsync(Booking booking);
+        Task<Booking?> GetBookingByIdAsync(int id);
+
+
+    }
+}
---
+++ BookingSystem.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static async Task Main()
+    {
+        
+    }
+}
